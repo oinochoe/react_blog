f53fc271381d15787099d2ce5f3d37abd2692299
yeongminkim <oinochoe@naver.com>
2020-04-09 00:37:21 +0900

    4000번 포트 사용하여 예시 koa

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,1 +1,10 @@
-const hello = 'hello';
+const Koa = require('koa');
+const app = new Koa();
+
+app.use((ctx) => {
+    ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+    console.log(`Server started on port 4000`);
+});
