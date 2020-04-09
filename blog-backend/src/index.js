535496a0621f1fafb7c94260eadef559644d6c7c
yeongminkim <oinochoe@naver.com>
2020-04-10 00:18:54 +0900

    라우터 설정

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,6 +1,20 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
+
+// 라우터 설정
+router.get('/', (ctx) => {
+    ctx.body = '홈';
+});
+
+router.get('/about', (ctx) => {
+    ctx.body = '소개';
+});
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log(`Server started on port 4000`);
