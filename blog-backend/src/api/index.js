cf3294e87d2d11ff9b1b416795d16bbcfc8a6849
yeongminkim <oinochoe@naver.com>
2020-04-13 01:22:09 +0900

    api 설정 및 koa bodyparser 설정

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,9 +1,9 @@
 const Router = require('koa-router');
+const posts = require('./posts');
+
 const api = new Router();
 
-api.get('/test', (ctx) => {
-    ctx.body = 'test 성공';
-});
+api.use('/posts', posts.routes());
 
 // 라우터를 내보냅니다.
 module.exports = api;
