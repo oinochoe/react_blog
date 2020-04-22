6619eb298162843017d4796d9f7a0b4c1f12dd0b
Yeongmin Kim <oinochoe@naver.com>
2020-04-22 23:44:44 +0900

    jsconfig로 vscode include 방식을 좀 더 수월하게

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -15,8 +17,6 @@ mongoose
     .catch((e) => {
         console.error(e);
     });
-
-const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
