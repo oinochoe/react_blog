93f6becca5a8e76282b019dcb86a0f1a155fda6b
yeongminkim <oinochoe@naver.com>
2020-04-24 00:15:20 +0900

    write 구현

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,6 +1,27 @@
 import Post from '../../models/posts';
 
-export const write = (ctx) => {};
+/*
+ * POST /api/posts
+ * {
+ *  title: '제목',
+ *  body: '내용',
+ *  tags: ['태그1', '태그2']
+ * }
+ */
+export const write = async (ctx) => {
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
 
 export const list = (ctx) => {};
 
