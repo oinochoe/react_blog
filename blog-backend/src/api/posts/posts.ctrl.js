d5dc4d3eb974c4d960a31ab77c602ad56b50539b
yeongminkim <oinochoe@naver.com>
2020-04-24 23:54:00 +0900

    지우기 기능 완료

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -51,7 +51,17 @@ export const read = async (ctx) => {
         ctx.throw(500, e);
     }
 };
-
-export const remove = (ctx) => {};
+/*
+ * DELETE /api/posts/:id
+ */
+export const remove = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch (error) {
+        ctx.throw(500, error);
+    }
+};
 
 export const update = (ctx) => {};
