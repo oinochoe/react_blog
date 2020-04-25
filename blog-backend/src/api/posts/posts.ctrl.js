737c92aea24a0579728e472946f1ffffae07a88b
yeongminkim <oinochoe@naver.com>
2020-04-26 02:41:30 +0900

    미들웨어 작성

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/post';
+import { mongoose } from 'mongoose';
+
+// 미들웨어 작성
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400; // Bad request
+        return;
+    }
+    return next();
+};
 
 /*
  * POST /api/posts
