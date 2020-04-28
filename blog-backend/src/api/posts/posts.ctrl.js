50abca65f4d3424d254cdc9d4d05357e1de2487a
yeongminkim <oinochoe@naver.com>
2020-04-29 01:19:14 +0900

    list sort 정렬 추가

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -57,7 +57,7 @@ export const write = async (ctx) => {
  */
 export const list = async (ctx) => {
     try {
-        const posts = await Post.find().exec();
+        const posts = await Post.find().sort({ _id: -1 }).exec();
         ctx.body = posts;
     } catch (e) {
         ctx.throw(500, e);
