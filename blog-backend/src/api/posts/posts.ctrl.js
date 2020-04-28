576d00e84ca4a0bae95344e5375478189429b809
yeongminkim <oinochoe@naver.com>
2020-04-29 01:25:41 +0900

    list 10개로 제한

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -57,7 +57,7 @@ export const write = async (ctx) => {
  */
 export const list = async (ctx) => {
     try {
-        const posts = await Post.find().sort({ _id: -1 }).exec();
+        const posts = await Post.find().sort({ _id: -1 }).limit(10).exec();
         ctx.body = posts;
     } catch (e) {
         ctx.throw(500, e);
