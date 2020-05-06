5eadda1d49c34f0eb6022b0e3853542f1301ea74
yeongminkim <oinochoe@naver.com>
2020-05-06 23:06:57 +0900

    setpassword, checkpassword 메서드 생성

--- blog-backend/src/models/user.js
+++ blog-backend/src/models/user.js
@@ -1,9 +1,20 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema({
     username: String,
     hashedPassword: String,
 });
+
+UserSchema.methods.setPassword = async function (password) {
+    const hash = await bcrypt.hash(password, 10);
+    this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+    const result = await bcrypt.compare(password, this.hashedPassword);
+    return result; // true / false
+};
 
 const User = mongoose.model('User', UserSchema);
 export default User;
