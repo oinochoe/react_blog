b7c318391bd6d58f1f447faeaec4982a19622a87
yeongminkim <oinochoe@naver.com>
2020-05-11 01:41:17 +0900

    login 구현

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -42,8 +42,40 @@ export const register = async (ctx) => {
         ctx.throw(500, error);
     }
 };
+
+/*
+ * POST /api/auth/login
+ * {
+ *  username: 'yeongmin',
+ *  password 'yeongmin'
+ * }
+ */
 export const login = async (ctx) => {
-    // 로그인
+    const { username, password } = ctx.request.body;
+
+    // username, password가 없으면 에러 처리
+    if (!username || !password) {
+        ctx.status = 401; // Unauthroized;
+        return;
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        // 계정이 존재하지 않으면 에러
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        // 잘못된 비밀번호
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+    } catch (error) {
+        ctx.throw(500, error);
+    }
 };
 export const check = async (ctx) => {
     // 로그인 상태 확인
