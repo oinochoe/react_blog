b62a9382f9c67c0f39167a53d680973a387467c9
yeongminkim <oinochoe@naver.com>
2020-05-13 02:27:28 +0900

    token generate 추가

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -38,6 +38,12 @@ export const register = async (ctx) => {
         await user.save(); // 데이터베이스에 저장
 
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+            httpOnly: true,
+        });
     } catch (error) {
         ctx.throw(500, error);
     }
@@ -73,6 +79,12 @@ export const login = async (ctx) => {
             return;
         }
         ctx.body = user.serialize();
+
+        const token = user.generateToken();
+        ctx.cookies.set('access_token', token, {
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+            httpOnly: true,
+        });
     } catch (error) {
         ctx.throw(500, error);
     }
