63aad9c26a5795bf030cee80db07bf80057c75ea
yeongminkim <oinochoe@naver.com>
2020-05-17 14:51:34 +0900

    토큰 재생성 관련 로직 완료

--- blog-backend/src/lib/jwtMiddleware.js
+++ blog-backend/src/lib/jwtMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/user';
 
-const jwtMiddleware = (ctx, next) => {
+const jwtMiddleware = async (ctx, next) => {
     const token = ctx.cookies.get('access_token');
     if (!token) return next(); // 토큰이 없음
     try {
@@ -9,7 +10,20 @@ const jwtMiddleware = (ctx, next) => {
             _id: decoded._id,
             username: decoded.username,
         };
+
+        // 토큰이 남은 유효 기간이 3.5일 미만이면 재발급
+        const now = Math.floor(Date.now() / 1000);
+        if (decoded.exp - now < 60 * 60 * 24 * 3.5) {
+            const user = await User.findById(decoded.id);
+            const token = user.generateToken();
+            ctx.cookies.set('access_token', token, {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+                httpOnly: true,
+            });
+        }
+
         console.log(decoded);
+
         return next();
     } catch (error) {
         // 토큰 검증 실패
