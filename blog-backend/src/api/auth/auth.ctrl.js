59f31ac718af8f643fedf59989566f34e230fb25
yeongminkim <oinochoe@naver.com>
2020-05-17 15:00:50 +0900

    로그아웃 기능 완료

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -103,6 +103,10 @@ export const check = async (ctx) => {
     ctx.body = user;
 };
 
+/*
+ * POST /api/auth/logout
+ */
 export const logout = async (ctx) => {
-    // 로그아웃
+    ctx.cookies.set('access_token');
+    ctx.status = 204; // No Content
 };
