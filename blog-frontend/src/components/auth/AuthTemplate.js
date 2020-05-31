b5733bb2674ef75279b6e818058253c6223e6770
yeongminkim <oinochoe@naver.com>
2020-06-01 00:04:01 +0900

    authTemplate children 렌더링

--- blog-frontend/src/components/auth/AuthTemplate.js
+++ blog-frontend/src/components/auth/AuthTemplate.js
@@ -7,8 +7,8 @@ import styled from 'styled-components';
 
 const AuthTemplateBlock = styled.div``;
 
-const AuthTemplate = () => {
-    return <AuthTemplateBlock />;
+const AuthTemplate = ({ children }) => {
+    return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
 };
 
 export default AuthTemplate;
