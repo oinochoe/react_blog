e10a7e9eb88e6c9c755058848ba72ecf21835f6f
yeongminkim <oinochoe@naver.com>
2020-06-02 00:13:10 +0900

    로그인폼 css 작업 완료

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -1,12 +1,81 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
 
 /* 회원가입 또는 로그인 폼을 보여 줍니다. */
 
-const AuthFormBlock = styled.div``;
+const AuthFormBlock = styled.div`
+    h3 {
+        margin: 0;
+        margin-bottom: 1rem;
+        color: ${palette.gray[8]};
+    }
+`;
+
+/**
+ * 스타일링된 input
+ */
+const StyledInput = styled.input`
+    width: 100%;
+    padding-bottom: 0.5rem;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid ${palette.gray[5]};
+    &:focus {
+        border-bottom: 1px solid ${palette.gray[7]};
+        color: $oc-teal-7;
+    }
+    & + & {
+        margin-top: 1rem;
+    }
+`;
+
+/**
+ * 폼 하단에 로그인 혹은 회원가입 링크를 보여 줌
+ */
+const Footer = styled.div`
+    margin-top: 2rem;
+    text-align: right;
+    a {
+        color: ${palette.gray[6]};
+        text-decoration: underline;
+        &:hover {
+            color: ${palette.gray[9]};
+        }
+    }
+`;
+
+const ButtonWithMarginTop = styled(Button)`
+    margin-top: 1rem;
+`;
 
 const AuthForm = () => {
-    return <AuthFormBlock>AuthForm</AuthFormBlock>;
+    return (
+        <AuthFormBlock>
+            <h3>로그인</h3>
+            <form>
+                <StyledInput
+                    autoComplete="username"
+                    name="username"
+                    placeholder="아이디"
+                />
+                <StyledInput
+                    type="password"
+                    autoComplete="new-password"
+                    name="password"
+                    placeholder="비밀번호"
+                />
+                <ButtonWithMarginTop fullWidth cyan>
+                    로그인
+                </ButtonWithMarginTop>
+            </form>
+            <Footer>
+                <Link to="/register">회원가입</Link>
+            </Footer>
+        </AuthFormBlock>
+    );
 };
 
 export default AuthForm;
