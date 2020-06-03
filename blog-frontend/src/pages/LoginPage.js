838da9ae1b4a59ca3712ab0b3c183929c971de86
yeongminkim <oinochoe@naver.com>
2020-06-04 00:06:09 +0900

    login form 만들기

--- blog-frontend/src/pages/LoginPage.js
+++ blog-frontend/src/pages/LoginPage.js
@@ -1,11 +1,11 @@
 ﻿import React from 'react';
 import AuthTemplate from '../components/auth/AuthTemplate';
-import AuthForm from '../components/auth/AuthForm';
+import LoginForm from '../components/auth/LoginForm';
 
 const LoginPage = () => {
     return (
         <AuthTemplate>
-            <AuthForm type="login" />
+            <LoginForm />
         </AuthTemplate>
     );
 };
