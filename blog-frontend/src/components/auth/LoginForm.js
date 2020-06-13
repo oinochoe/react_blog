4976fe91ae921a564e8c5e9c56e343e30b3cdee4
yeongminkim <oinochoe@naver.com>
2020-06-13 17:03:53 +0900

    에러 설정 완료

--- blog-frontend/src/components/auth/LoginForm.js
+++ blog-frontend/src/components/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { changeField, initializeForm, login } from '../../modules/auth';
@@ -6,6 +6,7 @@ import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
 
 const LoginForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
     const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.login,
@@ -40,8 +41,9 @@ const LoginForm = ({ history }) => {
 
     useEffect(() => {
         if (authError) {
-            console.log('오류발생');
+            console.log('오류 발생');
             console.log(authError);
+            setError('로그인 실패');
             return;
         }
         if (auth) {
@@ -62,6 +64,7 @@ const LoginForm = ({ history }) => {
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
