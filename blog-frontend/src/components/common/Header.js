bb61112718ca67c416e9f4e3d173cfe94ac9e758
yeongminkim <oinochoe@naver.com>
2020-06-14 04:46:55 +0900

    header 컴포넌트 제작중

---
+++ blog-frontend/src/components/common/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from './Responsive';
+import Button from './Button';
+
+const HeaderBlock = styled.div`
+    position: fixed;
+    width: 100%;
+`;
