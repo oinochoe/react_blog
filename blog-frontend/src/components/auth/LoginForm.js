b8f8a0e89312762f8d2dd8fb70cc39019bf2889c
yeongminkim <oinochoe@naver.com>
2020-06-14 16:06:26 +0900

    localstorage 사용해서 user 상태 저장해주기

--- blog-frontend/src/components/auth/LoginForm.js
+++ blog-frontend/src/components/auth/LoginForm.js
@@ -55,6 +55,11 @@ const LoginForm = ({ history }) => {
     useEffect(() => {
         if (user) {
             history.push('/');
+            try {
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (error) {
+                console.log('localStorage is not working', error);
+            }
         }
     }, [history, user]);
 
