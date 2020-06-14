2f8acdd75ede54e5d41cbe94b3bf156de9834986
yeongminkim <oinochoe@naver.com>
2020-06-14 15:57:18 +0900

    HeaderContainer 만들기

--- blog-frontend/src/components/common/Header.js
+++ blog-frontend/src/components/common/Header.js
@@ -37,7 +37,16 @@ const Spacer = styled.div`
     height: 4rem;
 `;
 
-const Header = () => {
+/**
+ * User 값에 따른 상태 변경
+ */
+
+const UserInfo = styled.div`
+    font-weight: 800;
+    margin-right: 1rem;
+`;
+
+const Header = ({ user }) => {
     return (
         <>
             <HeaderBlock>
@@ -45,9 +54,16 @@ const Header = () => {
                     <Link to="/" className="logo">
                         Reacters
                     </Link>
-                    <div className="right">
-                        <Button to="/login">로그인</Button>
-                    </div>
+                    {user ? (
+                        <div className="right">
+                            <UserInfo>{user.username}</UserInfo>
+                            <Button>로그아웃</Button>
+                        </div>
+                    ) : (
+                        <div className="right">
+                            <Button to="/login">로그인</Button>
+                        </div>
+                    )}
                 </Wrapper>
             </HeaderBlock>
             <Spacer />
--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from '../components/common/Button';
-import Header from '../components/common/Header';
+import HeaderContainer from '../components/common/HeaderContainer';
 
 const PostListPage = () => {
     return (
         <div>
-            <Header />
+            <HeaderContainer />
             <Button>포스트 리스트</Button>
         </div>
     );
