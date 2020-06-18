2f20dc11c94c6139fb7535eeffbff5092ae98d9a
yeongminkim <oinochoe@naver.com>
2020-06-18 18:44:56 +0900

    TagBox 추가

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,11 +1,13 @@
 ﻿import React from 'react';
-import Editor from '../components/write/Editor';
 import Responsive from '../components/common/Responsive';
+import Editor from '../components/write/Editor';
+import TagBox from '../components/write/TagBox';
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor />
+            <TagBox />
         </Responsive>
     );
 };
