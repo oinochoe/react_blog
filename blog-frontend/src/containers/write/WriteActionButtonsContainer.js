717b3285812a9a0acb380a0068b6c12cb59e8574
yeongminkim <oinochoe@naver.com>
2020-06-21 23:49:39 +0900

    write page 포스트 작성 성공

--- blog-frontend/src/containers/write/WriteActionButtonsContainer.js
+++ blog-frontend/src/containers/write/WriteActionButtonsContainer.js
@@ -1,20 +1,46 @@
 import React, { useEffect } from 'react';
-import { all } from 'redux-saga/effects';
-import auth, { authSaga } from './auth';
-import loading from './loading';
-import user, { userSaga } from './user';
-import write, { writeSaga } from './write';
-import { combineReducers } from 'redux';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
+import { useSelector, useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { writePost } from '../../modules/write';
 
-const rootReducer = combineReducers({
-    auth,
-    loading,
-    user,
-    write,
-});
+const WriteActionButtonsContainer = ({ history }) => {
+    const dispatch = useDispatch();
+    const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+        title: write.title,
+        body: write.body,
+        tags: write.tags,
+        post: write.post,
+        postError: write.postError,
+    }));
 
-const WriteActionButtonsContainer = () => {
-    return <div></div>;
+    // 포스트 등록
+    const onPublish = () => {
+        dispatch(
+            writePost({
+                title,
+                body,
+                tags,
+            }),
+        );
+    };
+
+    // 취소
+    const onCancel = () => {
+        history.goBack();
+    };
+
+    // 성공 혹은 실패 시 할 작업
+    useEffect(() => {
+        if (post) {
+            const { _id, user } = post;
+            history.push(`/@${user.username}/${_id}`);
+        }
+        if (postError) {
+            console.log(postError);
+        }
+    }, [history, post, postError]);
+    return <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />;
 };
 
-export default WriteActionButtonsContainer;
+export default withRouter(WriteActionButtonsContainer);
