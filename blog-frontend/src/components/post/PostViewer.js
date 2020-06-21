0975af1bafe18923b7378cc7e7ccec967a4f5121
yeongminkim <oinochoe@naver.com>
2020-06-22 00:37:14 +0900

    읽기 페이지 구현중

---
+++ blog-frontend/src/components/post/PostViewer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const PostViewer = () => {
+    return <div></div>;
+};
+
+export default PostViewer;
