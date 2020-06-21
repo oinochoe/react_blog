68c02183b05f139054458280568b702313bac7f2
yeongminkim <oinochoe@naver.com>
2020-06-21 13:47:49 +0900

    write editor 컨테이너

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,13 +1,13 @@
 ﻿import React from 'react';
 import Responsive from '../components/common/Responsive';
-import Editor from '../components/write/Editor';
+import EditorContainer from '../containers/write/EditorContainer';
 import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
+            <EditorContainer />
             <TagBox />
             <WriteActionButtons />
         </Responsive>
